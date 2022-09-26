df1f0fe5943246501b581426421abfd0f35b92cb
Siva Surya <sivasurya@ITs-Mac-mini.local>
2022-09-26 13:42:37 +0530

    initial commits

---
+++ src/main/java/CallMarshalling.java
@@ -0,0 +1,13 @@
+import
+public class CallMarshalling {
+
+    public static void main(String[] args) throws Exception {
+
+        Assignment2 obj = new Assignment2();
+        obj.detail();
+
+
+
+
+    }
+}
